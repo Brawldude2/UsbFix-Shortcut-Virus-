71dc0a4515c9a184325cda15d756296d99f19248
Brawldude2 <84960694+Brawldude2@users.noreply.github.com>
2021-09-19 12:00:02 +0300

    Updated version.

--- UsbFix.bat
+++ UsbFix.bat
@@ -1,19 +1,20 @@
-@echo off
+::Made by Brawldude2. ;)
 
+@echo off
 set randomfilename = "DRGHUF"
 
-SET /p drive="Usb harfini giriniz:"
+SET /p drive="Enter the removable drive letter:"
 CD /D "%drive%:\"
 
 if %errorlevel% == 0 (goto contin)
-echo "Surucu bulunamadi."
+echo "Drive cannot found."
 timeout /t 5
 exit
 
 :contin
 	attrib -r -h -s /s /d
 	del *.lnk
-	echo "Deleting lnk files."
+	echo "Deleting shortcut files."
 	set thereis=0
 	for /d %%f in (*) do (echo "%%~nf"|findstr /r "\<.\>"&&ren "%%f" "DRGHUF"&& set thereis=1)
 	
@@ -24,23 +25,26 @@ echo "Folder state: %thereis%"
 	if %thereis%==1 CD "DRGHUF"
 echo "Cd into the main folder."
 	
-	del autorun.inf
-	del desktop.ini
-echo "Deleting more stuff"
+	del "autorun.inf"
+	del "desktop.ini"
+echo "Deleting configuration and autorun files."
 
 	echo "Regex search..."
 	for /R %%f in (*) do (echo %%~nf|findstr /r "[-_][-_][-_][-_][-_]"&&del "%%f")
 	
 	if %thereis%==1 goto extract 
 :finish
-	md "Autorun.inf"
-	cd "Autorun.inf"
-	type nul>Policy.txt
+	if exist "autorun.inf" goto finish2
+	md "autorun.inf"
+	cd "autorun.inf"
+	echo "This file is created to perevent reinfections.">Policy.txt
 	cd..
-	attrib +r +h +s /s /d "Autorun.inf"
+
+:finish2
+	attrib +r +h +s /d "Autorun.inf"
 	attrib +r +h +s /s /d ^\.*
 	attrib +r +h +s /s /d "System Volume Information"
-	echo "Bitti :)"
+	echo "Done :)"
 	timeout /t 10
 	exit
 
