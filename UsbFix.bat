d1cc3df0dc0646a1bc6dce8241f5d08d16a42145
Brawldude2 <84960694+Brawldude2@users.noreply.github.com>
2021-09-23 19:59:34 +0300

    Updated.

--- UsbFix.bat
+++ UsbFix.bat
@@ -40,7 +40,8 @@ echo "Deleting configuration and autorun files."
 	cd..
 
 :finish2
-	attrib +r +h +s /d "Autorun.inf"
+	attrib +r +h +s /s /d "IndexerVolumeGuid"
+	attrib +r +h +s /s /d "Autorun.inf"
 	attrib +r +h +s /s /d ^\.*
 	attrib +r +h +s /s /d "System Volume Information"
 	echo "Done :)"
